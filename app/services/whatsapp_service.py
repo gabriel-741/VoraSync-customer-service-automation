fe6d4fc2ef17e967af131f21f3067809ab59029f
gabriel-741 <gabrielhenriqueghsb.24@gmail.com>
2026-05-29 15:24:11 -0300

    tirando o debug para evitar erro

--- app/services/whatsapp_service.py
+++ app/services/whatsapp_service.py
@@ -6,6 +6,9 @@ from app.utils.logger import get_logger
 
 log = get_logger(__name__)
 
+log.info(f"🔥 INSTANCE ATUAL: {settings.INSTANCE}")
+log.info(f"🔥 BASE_URL: {settings.BASE_URL}")
+log.info(f"🔥 API_KEY: {settings.API_KEY}")
 
 
 async def send_message(to: str, body: str) -> bool:
